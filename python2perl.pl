a9d78e9d4a6af97078e1b267e4fd29a06e972550
Zaw-Myo-Htun <jzhtun@gmail.com>
2014-09-29 17:22:42 +1000

    debug for subset 1

--- python2perl.pl
+++ python2perl.pl
@@ -2,10 +2,11 @@
 @variables = ();
 $pythonLine = "";
 while ($line = <>) {
+	chomp $line;
 	$pythonLine = $line;
 	subset0($line);
 	subset1($line);
-	print $pythonLine;
+	print "$pythonLine\n";
 }
 
 sub subset0{
@@ -13,7 +14,7 @@ sub subset0{
 if ($line =~ /^#!/ && $. == 1) {
 	
 		# translate #! line 
-		$pythonLine =  "#!/usr/bin/perl -w\n";
+		$pythonLine =  "#!/usr/bin/perl -w";
 	} elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
 	
 		# Blank & comment lines can be passed unchanged
@@ -21,16 +22,16 @@ if ($line =~ /^#!/ && $. == 1) {
 	} elsif ($line =~ /^\s*print\s*(.*)\s*$/) {
 		# Python's print print a new-line character by default
 		# so we need to add it explicitly to the Perl print statement
-		$pythonLine = "print \"$1\\n\";\n";
+		$pythonLine = "print \"$1\\n\";";
 	}
 }
 
 sub subset1{
 	$line = "@_";
-if ($line =~ /^[a-zA-Z_]*\s*=\s*/) {
+if ($line =~ /^[^0-9][a-zA-Z_0-9]*\s*=\s*/) {
+		$pythonLine .= ";";
 		$line =~ s/\s*=.*//;
 		if(not($line ~~ @variables)){
-		chomp $line;
 		push @variables, $line;
 		}
 	}
